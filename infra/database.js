f3926e93fdc1ff409bb07a6f0de8d33cc33d2559
kaiocesar0 <kcesar0910@gmail.com>
2025-12-03 08:35:47 -0300

    Adicionar um método de configuração SSL mais robusto

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
-  console.log("Credenciais do banco de dados:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
@@ -26,6 +20,16 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_SSL_CA) {
+    return {
+      ca: process.env.POSTGRES_SSL_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
 }
 
 export default {
