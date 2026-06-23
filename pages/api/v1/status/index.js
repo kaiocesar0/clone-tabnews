92eeb2eb3168fc484dc8cef20a1ca5b5e26f540f
kaiocesar0 <kcesar0910@gmail.com>
2026-06-23 07:42:03 -0300

    fix: extract major version from database version in status API

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,8 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseVersionValue =
+    databaseVersionResult.rows[0].server_version.split(" ")[0];
 
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
